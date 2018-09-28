5e2e24bd9ec0660a3724bb6f471fbf62e4ba313b
alexthayn <alexthayn>
2018-09-28 13:16:15 -0600

    created view model and models

---
+++ Model/GameObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Model
+{
+    public class GameObject
+    {
+        Point _position { get; set; }
+        Point _deltaPosition { get; set; }
+        double _heading { get; set; }
+        double _size { get; set; }
+
+    }
+}
